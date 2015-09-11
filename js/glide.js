f4c3aa8b7ed23d778c7f5432381bbcc95fd4f369
mstinson <markstinson@pc219.odos.uiuc.edu>
2015-09-11 14:35:50 -0500

    styles and scripts
    add styles and scripts

---
+++ js/glide.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	
+	$('.slider').glide({
+        autoplay: 7000,
+		 arrows: true,
+		 navigationCenter: true,
+		 arrowLeftText: "<span class='arrow'></span><span>prev</span>",
+		 arrowRightText: "<span class='arrow'></span><span>next</span>",
+    });
+	
+});
