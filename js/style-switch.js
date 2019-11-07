0e5d9b639afbebdf1754976cf948db00fe788344
Mark Stinson <stinson.mark@gmail.com>
2019-11-07 14:00:45 -0600

    Updates

--- js/style-switch.js
+++ js/style-switch.js
@@ -2,23 +2,23 @@
 $(document).ready(function() { 
 	
 	$("#navigation li.all a").click(function(){
-		$(".home-grid li").removeClass("show");
+		$("ul.folio li").removeClass("noshow");
 		$("#navigation li.all a").removeClass("current");
 		$("#navigation li.all a").removeClass("active");
-		$(".home-grid li.default").addClass("show");
+		$("ul.folio li.default").addClass("noshow");
 		$(this).addClass("current");
 	});
 		
 	$("#navigation li.print a").click(function(){
-		$(".home-grid li").removeClass("show");
+		$("ul.folio li").removeClass("noshow");
 		$("#navigation li.print a").removeClass("current");
 		$("#navigation li.print a").removeClass("active");
-		$(".home-grid li.print, .home-grid li.print-digital, .home-grid li.print-logos, .home-grid li.print-digital-logos").addClass("show");
+		$("ul.folio li.digital, ul.folio li.logos, ul.folio li.digital-logos").addClass("noshow");
 		$(this).addClass("current");
 	});
 	
 	$("#navigation li.digital a").click(function(){
-		$(".home-grid li").removeClass("show");
+		$("ul.home-grid li").removeClass("show");
 		$("#navigation li.digital a").removeClass("current");
 		$("#navigation li.digital a").removeClass("active");
 		$(".home-grid li.print, .home-grid li.logos").addClass("show");
@@ -26,7 +26,7 @@ $(document).ready(function() {
 	});
 	
 	$("#navigation li.logos a").click(function(){
-		$(".home-grid li").removeClass("show");
+		$("ul.home-grid li").removeClass("show");
 		$("#navigation li.logos a").removeClass("current");
 		$("#navigation li.logos a").removeClass("active");
 		$(".home-grid li.print, .home-grid li.digital").addClass("show");
